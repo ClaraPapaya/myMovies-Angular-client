6f580b6923d4c2c30200c6652843529a86547239
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-12-03 18:36:44 +0100

    Add user registration form and welcome screen

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input } from '@angular/core';
+// Closes the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// Brings in the API calls we created in 6.2
+import { UserRegistrationService } from '../fetch-api-data.service';
+// Used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  // Send the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      // Logic for a successful user registration goes here! (To be implemented)
+      this.dialogRef.close(); // Close the modal on success
+      console.log(result);
+      this.snackBar.open('User registration successful!', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(result)
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
